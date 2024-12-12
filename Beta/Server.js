1b4a9edef98af7a944b8b551d9e47d71d32ba2bc
Marco Colombara <devocolombaramarco@gmail.com>
2024-12-12 21:53:03 +0100

    IO

--- Beta/Server.js
+++ Beta/Server.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const { RoomPool, Room } = require('./Room')
+const { RoomPool } = require('./Room')
 const Rooms = new RoomPool()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Americaya was Here')
-});
+})
 
 app.post('/rooms/create/admin/:adminName', (req, res) => {
     const { adminName } = req.params
     const room = Rooms.Create(adminName)
     res.json(room)
-});
+})
 
 app.get('/rooms/:id', (req, res) => {
     const { id } = req.params
@@ -24,8 +24,8 @@ app.get('/rooms/:id', (req, res) => {
         return res.status(404).json({ error: 'Stanza non trovata.' });
     }
     res.json(room)
-});
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
-});
+})
